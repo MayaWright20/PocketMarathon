b741fd510b30c566c9a8fb62b030c69c0b1a3679
mayagwright20 <mayagwright20@hotmail.com>
2023-12-15 23:13:40 +0000

    dragable itervals added - now need to fix stYLING

--- Components/CustomRun/Sections/Intervals/IntervalsList.tsx
+++ Components/CustomRun/Sections/Intervals/IntervalsList.tsx
@@ -1,157 +1,192 @@
-import React, { useContext } from "react";
+import React, { useContext, memo, useCallback, useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
+import DraggableGridView from 'react-native-drag-sort-gridview'
+
 import { SCREEN_WIDTH } from "../../../../Constants/DIMENSIONS";
 import { OptionsContext } from "../../../../Context/CustomRunContext/OptionsContext";
 import SquareCTAButton from "../../Buttons/SquareCTAButton";
 import { HEADER_1 } from "../../../../Constants/Styling/STYLES";
-import { ScrollView } from "react-native-gesture-handler";
 import { COLORS } from "../../../../Constants/COLORS";
 
-export default function IntervalsList() {
-    const optionsCtx = useContext(OptionsContext);
-    const intervalsArr = optionsCtx.intervalsArr.slice(1);
-    const buttonDimensions = SCREEN_WIDTH / 4.2;
-    const emojiSize = 25;
-    const titleSize = 14;
-    console.log('j')
+const emojiSize = 25;
+const titleSize = 14;
 
-    function colorMaker(item: any){
-        let color = '';
-        let color2 = '';
-        if(item?.SPEED){
-            color = COLORS.MEDIUM_BLUE;
-            if(item?.TIME){
-                color2 = COLORS.PINK;
-            };
+function colorMaker(item: any) {
 
-            if(item?.DISTANCE){
-                color2 = COLORS.GREEN;
-            };
+    let color = '';
+    let color2 = '';
 
-            return {color, color2}
-        };
-
-        if(item?.TIME){
-            color = COLORS.PINK;
-            if(item?.DISTANCE){
-                color2 = COLORS.GREEN;
-            };
-            return {color, color2};
-        };
-
-       return {color, color2};
-    }
-
-    function titleMaker(item: any) {
-
-        console.log('item', item)
-        let title = '';
-        if (item?.SPEED) {
-            title = `🏎️ ${item?.SPEED}\n`;
-        }
-
+    if (item?.SPEED) {
+        color = COLORS.MEDIUM_BLUE;
         if (item?.TIME) {
-            if (item?.TIME?.HOURS !== undefined) {
-                title = title + `⏱️ ${item?.TIME?.HOURS}:`
-            } else {
-                title = title + '⏱️ 00:'
-            }
-
-            if (item?.TIME?.MINS !== undefined) {
-                title = title + `${item?.TIME?.MINS}:`
-            } else {
-                title = title + '00:'
-            }
-
-            if (item?.TIME?.SECS !== undefined) {
-                title = title + `${item?.TIME?.SECS}\n`
-            } else {
-                title = title + '00\n'
-            }
-        }
+            color2 = COLORS.PINK;
+        };
 
         if (item?.DISTANCE) {
-            if (item?.DISTANCE?.MILES !== undefined) {
-                title = title + `📏 ${item?.DISTANCE?.MILES}:`
-            } else {
-                title = title + '📏 00:'
-            }
+            color2 = COLORS.GREEN;
+        };
 
-            if (item?.DISTANCE?.KMS !== undefined) {
-                title = title + `${item?.DISTANCE?.KMS}:`
-            } else {
-                title = title + '00:'
-            }
+        return { color, color2 }
+    };
 
-            if (item?.DISTANCE?.METRES !== undefined) {
-                title = title + `${item?.DISTANCE?.METRES}\n`
-            } else {
-                title = title + '00\n'
-            }
+    if (item?.TIME) {
+        color = COLORS.PINK;
+        if (item?.DISTANCE) {
+            color2 = COLORS.GREEN;
+        };
+        return { color, color2 };
+    };
 
-        }
-        return title;
+    return { color, color2 };
+}
+
+function titleMaker(item: any) {
+
+    let title = '';
+
+    if (item?.SPEED) {
+        title = `🏎️ ${item?.SPEED}\n`;
+    };
+
+    if (item?.TIME) {
+        if (item?.TIME?.HOURS !== undefined) {
+            title = title + `⏱️ ${item?.TIME?.HOURS}:`
+        } else {
+            title = title + '⏱️ 00:'
+        };
+
+        if (item?.TIME?.MINS !== undefined) {
+            title = title + `${item?.TIME?.MINS}:`
+        } else {
+            title = title + '00:'
+        };
+
+        if (item?.TIME?.SECS !== undefined) {
+            title = title + `${item?.TIME?.SECS}\n`
+        } else {
+            title = title + '00\n'
+        };
+    };
+
+    if (item?.DISTANCE) {
+        if (item?.DISTANCE?.MILES !== undefined) {
+            title = title + `📏 ${item?.DISTANCE?.MILES}:`
+        } else {
+            title = title + '📏 00:'
+        };
+
+        if (item?.DISTANCE?.KMS !== undefined) {
+            title = title + `${item?.DISTANCE?.KMS}:`
+        } else {
+            title = title + '00:'
+        };
+
+        if (item?.DISTANCE?.METRES !== undefined) {
+            title = title + `${item?.DISTANCE?.METRES}\n`
+        } else {
+            title = title + '00\n'
+        };
 
     };
 
+    return title;
+
+};
+
+const Item = memo(({ item }: { item: any }) => (
+    <View style={styles.item}>
+        <SquareCTAButton
+            linearGradientColor1={COLORS.LIGHT_ORANGE}
+            linearGradientColor2={item ? colorMaker(item).color2 : 'yellow'}
+            title={titleMaker(item)}
+            overlayColor={""}
+            onPress={() => undefined}
+            width={SCREEN_WIDTH / 4.2} 
+            height={SCREEN_WIDTH / 4.2} 
+            emojiSize={emojiSize}
+            titleSize={titleSize}
+        />
+    </View>
+));
+  
+export default function IntervalsList() {
+    
+    const optionsCtx = useContext(OptionsContext);
+    useEffect(() => {
+        setData(optionsCtx.intervalsArr);
+    }, [optionsCtx.intervalsArr]);
+
+    const [data, setData] = useState(optionsCtx.intervalsArr);
+    
+    
+    const onOrderChanged = useCallback((orderedData: Array<any>) => {
+        setData(orderedData);
+    }, []);
+
+    const renderItem = ({ item }: { item: any }) => <Item item={item} />
+    const keyExtractor = ({ id }: any) => `gridview-${id}`;
+
+    
+
     return (
-        <ScrollView showsHorizontalScrollIndicator={false} horizontal style={{ display: optionsCtx.intervalsArr.length < 2 ? 'none' : 'flex' }}>
-            <View style={styles.container}>
-                <SquareCTAButton
-                    emoji={'📣'}
-                    linearGradientColor1={COLORS.LIGHT_ORANGE}
-                    linearGradientColor2={COLORS.ORANGE}
-                    title={"START"}
-                    overlayColor={""}
-                    onPress={() => undefined}
-                    width={buttonDimensions}
-                    height={buttonDimensions}
-                    emojiSize={emojiSize}
-                    titleSize={titleSize}
-                />
-                <View style={styles.intervalListWrapper}>
-                    {intervalsArr.map((item, index) =>
-                        // key={index} needs to change
-                        <View key={index}>
-                            <SquareCTAButton
-                                linearGradientColor1={item ? colorMaker(item).color : 'yellow'}
-                                linearGradientColor2={item ? colorMaker(item).color2 : 'blue'}
-                                title={titleMaker(item)}
-                                overlayColor={""}
-                                onPress={() => undefined}
-                                width={buttonDimensions}
-                                height={buttonDimensions}
-                                emojiSize={emojiSize}
-                                titleSize={titleSize}
-                            />
-                        </View>
-                    )}
-                </View>
-                <SquareCTAButton
-                    emoji={'🏁'}
-                    linearGradientColor1={COLORS.LIGHT_ORANGE}
-                    linearGradientColor2={COLORS.ORANGE}
-                    title={"FINISH"} overlayColor={""}
-                    onPress={() => undefined}
-                    width={buttonDimensions}
-                    height={buttonDimensions}
-                    emojiSize={emojiSize}
-                    titleSize={titleSize}
-                />
-            </View>
-        </ScrollView>
+        <View style={styles.container}>
+            <SquareCTAButton
+            linearGradientColor1={COLORS.LIGHT_ORANGE}
+            linearGradientColor2={COLORS.ORANGE}
+            title={'START'}
+            emoji={'📣'}
+            overlayColor={""}
+            onPress={() => undefined}
+            width={SCREEN_WIDTH / 4.2} 
+            height={SCREEN_WIDTH / 4.2} 
+            emojiSize={emojiSize}
+            titleSize={titleSize}
+        />
+        <DraggableGridView
+            // style={styles.bg}
+            // contentContainerStyle={styles.contentContainer}
+            // itemContainerStyle={styles.itemContainer}
+            isEditing={true}
+            numColumns={2}
+            // itemHeight={50}
+            data={data}
+            shouldAnimOnRelease={true}
+            keyExtractor={keyExtractor}
+            onOrderChanged={onOrderChanged}
+            renderItem={renderItem}
+            horizontal={true}
+            shouldVibrate={false}
+            // shouldAnimOnRelease={true}
+        />
+        <SquareCTAButton
+            linearGradientColor1={COLORS.LIGHT_ORANGE}
+            linearGradientColor2={COLORS.ORANGE}
+            title={'FINISH'}
+            emoji={'🏁'}
+            overlayColor={""}
+            onPress={() => undefined}
+            width={SCREEN_WIDTH / 4.2} 
+            height={SCREEN_WIDTH / 4.2} 
+            emojiSize={emojiSize}
+            titleSize={titleSize}
+        />
+        </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        marginVertical: 20,
+        marginVertical: 50,
+        marginHorizontal: 50,
         paddingLeft: 15,
         flexDirection: 'row',
         alignItems: 'center'
     },
     h1: HEADER_1,
-    intervalListWrapper: {
-        flexDirection: 'row',
+    item:{
+        backgroundColor : 'yellow',
+        // alignItems: 'stretch',
+        marginRight: 1
     }
+    
 });
