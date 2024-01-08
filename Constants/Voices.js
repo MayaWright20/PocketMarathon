34b73b72cd3f6fa5782ffb857ce81b433c501de1
mayagwright20 <mayagwright20@hotmail.com>
2024-01-08 20:59:08 +0000

    added voices for ios and a local env file

---
+++ Constants/Voices.js
@@ -0,0 +1,16 @@
+export const Voices = [
+    "ios" = {
+        "Serena": "com.apple.voice.premium.en-GB.Serena",
+        "Malcolm": "com.apple.voice.premium.en-GB.Malcolm",
+        "Oliver": "com.apple.voice.enhanced.en-GB.Oliver",
+        "Staphanie": "com.apple.voice.enhanced.en-GB.Stephanie",
+        "Cameron": "com.apple.voice.enhanced.en-GB.Malcolm",
+        "Daniel": "com.apple.voice.enhanced.en-GB.Daniel",
+        "Kate": "com.apple.voice.enhanced.en-GB.Kate",
+        "Amelie": "com.apple.voice.premium.fr-CA.Amelie",
+        "Nicolas": "com.apple.voice.enhanced.fr-CA.Nicolas"
+    },
+    "android" = {
+        WHITE: 'rgb(255,255,255)',
+    }
+];
