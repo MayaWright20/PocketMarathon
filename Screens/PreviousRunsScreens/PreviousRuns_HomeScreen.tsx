7e54dff916b81a421828219ec88349cffd4fad06
mayagwright20 <mayagwright20@hotmail.com>
2024-01-09 14:49:59 +0000

    added text to speech native plugin and no longer using expo

--- Screens/PreviousRunsScreens/PreviousRuns_HomeScreen.tsx
+++ Screens/PreviousRunsScreens/PreviousRuns_HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import Tts from 'react-native-tts';
 import * as FileSystem from 'expo-file-system';
 import { Audio } from 'expo-av';
 //59 mins
@@ -11,51 +12,24 @@ import { writeAudioToFile } from "../../utils/General/writeAudioFile";
 import { playFromPath } from "../../utils/General/playFromPath";
 
 
-Audio.setAudioModeAsync({
-    allowsRecordingIOS: false,
-    staysActiveInBackground: true,
-    playsInSilentModeIOS: true,
-    shouldDuckAndroid: true,
-    playThroughEarpieceAndroid: false
-});
 
 export default function PreviousRuns_HomeScreen() {
 
-    async function onPressHandler(text: string) {
+    function onPressHandler(){
+        Tts.setDucking(true);
+        Tts.setDefaultRate(0.6);
+        Tts.setDefaultPitch(1.5);
+        Tts.speak('Hello,dfds world!', {
+            iosVoiceId: 'com.apple.ttsbundle.Moira-compact',
+            rate: 0.5,
+          });
+    }
 
-        if (!text) return;
-
-        try{
-            console.log('inside try block')
-            const audioBlob = await fetchAudio(text);
-            console.log('inside try block fetchAudio complete')
-            const reader = new FileReader();
-
-            reader.onload = async (e) => {
-                if( e.target && typeof e.target.result === "string"){
-                    const audioData = e.target.result.split(",")[1];
-
-                    //save data
-                    const path = await writeAudioToFile(audioData);
-
-                    //play audio
-                    await playFromPath(path);
-                    
-                }
-            };
-
-            reader.readAsDataURL(audioBlob);
-
-        }catch(error){
-            console.error(error);
-            console.log('ERROR HAS HAPPENED')
-        }
-    };
 
     return (
         <ScreenLinearBackground>
             <Text>PreviousRuns_HomeScreen</Text>
-            <Button title="Send message" onPress={() => onPressHandler("hi test hi hihow are you! Great day aye. I'm Maya and I just got the google API working. HOW EXCITING!!! SHAME it sounds like a robot still. All this work for it to just sound like a bloody robot!")} />
+            <Button title="Send message" onPress={onPressHandler} />
         </ScreenLinearBackground>
     )
 };
