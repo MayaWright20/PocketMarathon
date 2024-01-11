5ed9eadef32caba25d80d6f862cf9300753cff2a
mayagwright20 <mayagwright20@hotmail.com>
2024-01-11 11:04:06 +0000

    revert .env file

--- Screens/PreviousRunsScreens/PreviousRuns_HomeScreen.tsx
+++ Screens/PreviousRunsScreens/PreviousRuns_HomeScreen.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+<<<<<<< HEAD
 // import Tts from 'react-native-tts';
+=======
+>>>>>>> parent of 7e54dff (added text to speech native plugin and no longer using expo)
 import * as FileSystem from 'expo-file-system';
 import { Audio } from 'expo-av';
 import { fetchAudio } from "../../utils/General/fetchAudio";
@@ -11,9 +14,17 @@ import { writeAudioToFile } from "../../utils/General/writeAudioFile";
 import { playFromPath } from "../../utils/General/playFromPath";
 
 
+Audio.setAudioModeAsync({
+    allowsRecordingIOS: false,
+    staysActiveInBackground: true,
+    playsInSilentModeIOS: true,
+    shouldDuckAndroid: true,
+    playThroughEarpieceAndroid: false
+});
 
 export default function PreviousRuns_HomeScreen() {
 
+<<<<<<< HEAD
     // function onPressHandler(){
     //     Tts.setDucking(true);
     //     Tts.setDefaultRate(0.6);
@@ -24,12 +35,47 @@ export default function PreviousRuns_HomeScreen() {
     //       });
     //     console.log('pressed button for text to speech')
     // }
+=======
+    async function onPressHandler(text: string) {
+>>>>>>> parent of 7e54dff (added text to speech native plugin and no longer using expo)
 
+        if (!text) return;
+
+        try{
+            console.log('inside try block')
+            const audioBlob = await fetchAudio(text);
+            console.log('inside try block fetchAudio complete')
+            const reader = new FileReader();
+
+            reader.onload = async (e) => {
+                if( e.target && typeof e.target.result === "string"){
+                    const audioData = e.target.result.split(",")[1];
+
+                    //save data
+                    const path = await writeAudioToFile(audioData);
+
+                    //play audio
+                    await playFromPath(path);
+                    
+                }
+            };
+
+            reader.readAsDataURL(audioBlob);
+
+        }catch(error){
+            console.error(error);
+            console.log('ERROR HAS HAPPENED')
+        }
+    };
 
     return (
         <ScreenLinearBackground>
             <Text>PreviousRuns_HomeScreen</Text>
+<<<<<<< HEAD
             {/* <Button title="Send message" onPress={onPressHandler} /> */}
+=======
+            <Button title="Send message" onPress={() => onPressHandler("hi test hi hihow are you! Great day aye. I'm Maya and I just got the google API working. HOW EXCITING!!! SHAME it sounds like a robot still. All this work for it to just sound like a bloody robot!")} />
+>>>>>>> parent of 7e54dff (added text to speech native plugin and no longer using expo)
         </ScreenLinearBackground>
     )
 };
