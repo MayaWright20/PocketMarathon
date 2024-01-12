c6077db4008f8fb09e7f46d4d1892efd39eb7b4d
mayagwright20 <mayagwright20@hotmail.com>
2024-01-12 14:24:01 +0000

    expo location with native tts working npx run ios is the command for running developement build with expo modules

--- Screens/PreviousRunsScreens/PreviousRuns_HomeScreen.tsx
+++ Screens/PreviousRunsScreens/PreviousRuns_HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import Tts from 'react-native-tts';
 import * as FileSystem from 'expo-file-system';
+import * as Speech from 'expo-speech';
 import { Audio } from 'expo-av';
 import { fetchAudio } from "../../utils/General/fetchAudio";
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from "../../Constants/DIMENSIONS";
@@ -11,25 +12,40 @@ import { writeAudioToFile } from "../../utils/General/writeAudioFile";
 import { playFromPath } from "../../utils/General/playFromPath";
 
 
-Audio.setAudioModeAsync({
-    allowsRecordingIOS: false,
-    staysActiveInBackground: true,
-    playsInSilentModeIOS: true,
-    shouldDuckAndroid: true,
-    playThroughEarpieceAndroid: false
-});
+// Audio.setAudioModeAsync({
+//     allowsRecordingIOS: false,
+//     staysActiveInBackground: true,
+//     playsInSilentModeIOS: true,
+//     shouldDuckAndroid: true,
+//     playThroughEarpieceAndroid: false
+// });
 
 export default function PreviousRuns_HomeScreen() {
 
-    function onPressHandler(){
+    // function onPressHandler(){
+    //     Speech.speak('hellow')
+    //     // Tts.setDucking(true);
+    //     // Tts.setDefaultPitch(1.5);
+    //     // Tts.speak('Hello, you world!', {
+    //     //     iosVoiceId: 'com.apple.ttsbundle.Moira-compact',
+    //     //     rate: 0.5,
+    //     //   });
+    //     //   Tts.voices().then(voices => console.log('voices',voices));
+    // };
+
+   
+
+      async function speak(){
+        // const thingToSay = '1 for every no thing today hi';
+        // Speech.speak(thingToSay);
+        // console.log(await Speech.isSpeakingAsync())
         Tts.setDucking(true);
-        Tts.setDefaultPitch(1.5);
-        Tts.speak('Hello,dfds world!', {
-            iosVoiceId: 'com.apple.voice.compact.en-GB.Daniel',
+        Tts.setDefaultPitch(1.2);
+        Tts.speak('Hello, you world!', {
+            iosVoiceId: 'com.apple.ttsbundle.Moira-compact',
             rate: 0.5,
           });
-        console.log('pressed button for text to speech')
-    };
+      }
 
        
     
@@ -37,8 +53,8 @@ export default function PreviousRuns_HomeScreen() {
 
     return (
         <ScreenLinearBackground>
-            <Text>PreviousRuns_HomeScreen</Text>
-            <Button title="Send message" onPress={()=>onPressHandler} />
+            <Text>PreviousRuns_HomeScreen hi</Text>
+            <Button title="Send message" onPress={speak} />
         </ScreenLinearBackground>
     )
 };
