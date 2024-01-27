82c4c961a3e051bc44f7771beb2ef3113334f852
mayagwright20 <mayagwright20@hotmail.com>
2024-01-27 17:10:58 +0000

    refactor so that arr[0] is not played twice. setNum(prev => prev + 1); has to be before setCounter(prevCounter => prevCounter - 1);

--- Screens/ProfilesScreens/Profile_HomeScreen.tsx
+++ Screens/ProfilesScreens/Profile_HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Text, StyleSheet, Button } from "react-native";
 
 import ScreenLinearBackground from "../../Constants/Styling/ScreenLinearBackground";
@@ -8,36 +8,36 @@ export default function Profile_HomeScreen() {
   const [isRunning, setIsRunning] = useState(true);
   const [counter, setCounter] = useState(3);
   const [num, setNum] = useState(0);
-  
-  let timer: any;
-  let arr = [{ 1000: "this is one how are you today i hope youre having a good day"}, { 1000: "this is two"},{ 1000: "this is three" }];
 
-useEffect(() => {
-  if (!isRunning) {
-     clearInterval(timer);
-     Tts.pause();
-     return;
-  }else{
-    Tts.resume();
-  };
- 
-  timer = counter > 0 && setInterval(() => {
-     Tts.speak(String(Object.values(arr[num])))
-     setCounter(prevCounter => prevCounter - 1);
-     setNum(prev => prev + 1);
-     console.log('counter', counter, 'num', num, 'arr', arr[num]);
-  }, Number(Object.keys(arr[num])));
- 
-  return () => {
-     clearInterval(timer);
-  };
- }, [counter, isRunning]);
- 
+
+  let timer: any;
+  let arr = [{ 1000: "this is one how are you today i hope youre having a good day" }, { 1000: "this is two" }, { 1000: "this is three" }];
+
+  useEffect(() => {
+    if (!isRunning) {
+      clearInterval(timer);
+      Tts.pause();
+      return;
+    } else {
+      Tts.resume();
+    };
+
+    timer = counter > 0 && setInterval(() => {
+      Tts.speak(String(Object.values(arr[num])));
+      setNum(prev => prev + 1);
+      setCounter(prevCounter => prevCounter - 1);
+    }, Number(Object.keys(arr[num])));
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [counter, isRunning]);
+
 
   return (
     <ScreenLinearBackground>
       <Text>{counter}</Text>
-      <Button title="Press" onPress={() => setIsRunning(!isRunning)}/>
+      <Button title="Press" onPress={() => setIsRunning(!isRunning)} />
     </ScreenLinearBackground>
   )
 };
