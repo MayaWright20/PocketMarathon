b5fa4bf64e2ed94de226f89138f01aba1cb54e5c
mayagwright20 <mayagwright20@hotmail.com>
2024-01-27 15:56:45 +0000

    timer and pause functionality for timeIntervals implemented on Profile_HomeScreen.tsx

--- Screens/ProfilesScreens/Profile_HomeScreen.tsx
+++ Screens/ProfilesScreens/Profile_HomeScreen.tsx
@@ -1,13 +1,43 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React, {useEffect, useState} from "react";
+import { Text, StyleSheet, Button } from "react-native";
 
 import ScreenLinearBackground from "../../Constants/Styling/ScreenLinearBackground";
+import Tts from "react-native-tts";
 
 export default function Profile_HomeScreen() {
+  const [isRunning, setIsRunning] = useState(true);
+  const [counter, setCounter] = useState(3);
+  const [num, setNum] = useState(0);
+  
+  let timer: any;
+  let arr = [{ 1000: "this is one how are you today i hope youre having a good day"}, { 20000: "this is two"},{ 6000: "this is three" }];
+
+useEffect(() => {
+  if (!isRunning) {
+     clearInterval(timer);
+     Tts.pause();
+     return;
+  }else{
+    Tts.resume();
+  };
+ 
+  timer = counter > 0 && setInterval(() => {
+     Tts.speak(String(Object.values(arr[num])))
+     setCounter(prevCounter => prevCounter - 1);
+     setNum(prev => prev + 1);
+     console.log('counter', counter, 'num', num, 'arr', arr[num]);
+  }, Number(Object.keys(arr[num])));
+ 
+  return () => {
+     clearInterval(timer);
+  };
+ }, [counter, isRunning]);
+ 
 
   return (
     <ScreenLinearBackground>
-      <Text>Profiled_HomeScreen</Text>
+      <Text>{counter}</Text>
+      <Button title="Press" onPress={() => setIsRunning(!isRunning)}/>
     </ScreenLinearBackground>
   )
 };
